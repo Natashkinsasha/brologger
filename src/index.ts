a156c599c6de575a8ed37aaa85d438d5e2a65601
Alex <natashkinsasha@whitesharx.com>
2019-11-15 16:09:10 +0300

    v 2.0

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import ConsoleTransport, { Colors, ConsoleTransportOptions } from "./ConsoleTran
 import Transport, { TransportOptions } from "./Transport";
 import ITransport from "./ITransport";
 import Log from "./Log";
+import ILogger from "./ILogger";
 
-export {Logger, ConsoleTransport, Transport, Log, ITransport, TransportOptions, ConsoleTransportOptions, Colors, Levels}
+export {ConsoleTransport, Transport, Log, ITransport, TransportOptions, ConsoleTransportOptions, Colors, Levels, ILogger}
 
+export default Logger;
--- test/Logger.test.ts
+++ test/Logger.test.ts
@@ -1,5 +1,5 @@
 import * as chai from 'chai';
-import {Logger} from '../src/index'
+import Logger from '../src/index'
 
 
 describe('#Logger', () => {
