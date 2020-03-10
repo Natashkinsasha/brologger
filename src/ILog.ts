7a253bedd425c60ec0287a0c9717c523ebd3f52a
Alex <natashkinsasha@whitesharx.com>
2020-03-10 17:41:38 +0300

    refactor

--- src/ILog.ts
+++ src/ILog.ts
@@ -5,11 +5,11 @@ export default interface ILog{
     message(message: string): ILog;
     meta(meta: object): ILog;
     object(object: object): ILog;
-    trace(): void;
-    debug(): void;
-    info(): void;
-    warn(): void;
-    error(): void;
-    fatal(): void;
-    log(level: string);
+    trace(): Promise<void>;
+    debug(): Promise<void>;
+    info(): Promise<void>;
+    warn(): Promise<void>;
+    error(): Promise<void>;
+    fatal(): Promise<void>;
+    log(level: string): Promise<void>;
 }
