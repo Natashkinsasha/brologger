9e4ced36d155c7cf1356da0244526cd3e3411c16
Alex <natashkinsasha@whitesharx.com>
2020-03-10 17:37:57 +0300

    refactor

--- src/ILogger.ts
+++ src/ILogger.ts
@@ -6,6 +6,6 @@ export default interface ILogger{
     message(message: string): Log;
     meta(meta: object): Log;
     object(object: object): Log;
-    log(level: string, message?: string, infoObject?: object, meta?: object): void
+    log(level: string, message?: string, infoObject?: object, meta?: object): Promise<void>
 
 }
--- src/Log.ts
+++ src/Log.ts
@@ -16,32 +16,32 @@ export default class Log implements ILog {
         this.infoObject = object;
     }
 
-    public debug(): void {
-        this.logger.log('debug', this.msg, this.infoObject, this.metaObject)
+    public debug(): Promise<void> {
+        return this.logger.log('debug', this.msg, this.infoObject, this.metaObject)
     }
 
-    public error(): void {
-        this.logger.log('error', this.msg, this.infoObject, this.metaObject)
+    public error(): Promise<void> {
+        return this.logger.log('error', this.msg, this.infoObject, this.metaObject)
     }
 
-    public fatal(): void {
-        this.logger.log('fatal', this.msg, this.infoObject, this.metaObject)
+    public fatal(): Promise<void> {
+        return this.logger.log('fatal', this.msg, this.infoObject, this.metaObject)
     }
 
-    public info(): void {
-        this.logger.log('info', this.msg, this.infoObject, this.metaObject)
+    public info(): Promise<void> {
+        return this.logger.log('info', this.msg, this.infoObject, this.metaObject)
     }
 
-    public trace(): void {
-        this.logger.log('trace', this.msg, this.infoObject, this.metaObject)
+    public trace(): Promise<void> {
+        return this.logger.log('trace', this.msg, this.infoObject, this.metaObject)
     }
 
-    public warn(): void {
-        this.logger.log('warn', this.msg, this.infoObject, this.metaObject)
+    public warn(): Promise<void> {
+        return this.logger.log('warn', this.msg, this.infoObject, this.metaObject)
     }
 
-    public log(level: string) {
-        this.logger.log(level, this.msg, this.infoObject, this.metaObject)
+    public log(level: string): Promise<void> {
+        return this.logger.log(level, this.msg, this.infoObject, this.metaObject)
     }
 
 
